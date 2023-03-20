1983215e07734ff657226e237029cbe39cf45ff3
Geraldo de Figueiredo Junior <geraldo.junior@eldorado.org.br>
2023-03-20 17:09:37 -0300

    Added functionality for local storage provider

--- src/app/services/database/provider/i-provider.ts
+++ src/app/services/database/provider/i-provider.ts
@@ -1,2 +1,8 @@
 export interface IProvider {
+  connect(): void;
+  disconnect(): void;
+  insert(table: string, data: any): void;
+  update(table: string, data: any, search: any): void;
+  delete(table: string, search: any): void;
+  select(table: string, search: any): Array<any>;
 }
